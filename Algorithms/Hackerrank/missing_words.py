8e4ad6358ed76fbb397f27510a389a9f904079ae
Tyrone <tyronejd10bs08@gmail.com>
2018-09-26 14:56:49 +0200

    Update missing_words.py

--- Algorithms/Hackerrank/missing_words.py
+++ Algorithms/Hackerrank/missing_words.py
@@ -2,23 +2,25 @@
 def missingWords(s, t):
     s = s.strip()
     t = t.strip()
-    s = s.split()
+    s = s.split() #convert s and t to lists
     t = t.split()
     missing_words = list()
     t_num_ocur = dict()
-    for word in t:
+    
+    for word in t:                             #
         if(word in t_num_ocur.keys()):
             t_num_ocur[word] += 1
         else:
             t_num_ocur[word] = 1
-    for i in range(len(s)):
-        if(s[i] in t_num_ocur.keys()):
-            if(t_num_ocur[s[i]] != 0):
-                t_num_ocur[s[i]] -= 1
+            
+    for word in s:
+        if(word in t_num_ocur.keys()):
+            if(t_num_ocur[word] != 0):
+                t_num_ocur[word] -= 1
             else:
-                missing_words.append(s[i])
+                missing_words.append(word)
         else:
-                missing_words.append(s[i])
+                missing_words.append(word)
     return missing_words 
     
-print(missingWords("          I am using HackerRank to improve programming", "chocolates chocolates"))
+print(missingWords("I am using HackerRank to improve programming", "I using programming"))
